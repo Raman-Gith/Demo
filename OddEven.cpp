0d92a8e0ce8d647b4e7c79a1e6d9020556c94bfc
Raman-Gith <99887112+Raman-Gith@users.noreply.github.com>
2022-02-18 18:03:53 +0530

    Add files to repository via upload

---
+++ OddEven.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+  int n;
+  cout<<"Enter number :"<<endl;
+  cin>>n;
+  
+  if(n%2==0)
+    cout<<"Even"<<endl;
+  else
+    cout<<"odd"<<endl;
+} 
